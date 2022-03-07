e127190c5457232e248c832fd7b525eaedf6a3fc
liquan <liquan@kuaishou.com>
2022-03-07 11:27:32 +0800

    feat: add web monorepo template

---
+++ node/scripts/release.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const { minimist, run, logger } = require('@eljs/node-utils')
+const { step, release } = require('@eljs/release')
+
+const args = minimist(process.argv.slice(2))
+const skipTests = args.skipTests
+const skipBuild = args.skipBuild
+
+main()
+
+async function main() {
+  const { stdout } = await run('git', ['status', '--porcelain'], {
+    stdio: 'pipe',
+  })
+
+  if (stdout) {
+    logger.printErrorAndExit('Your git status is not clean. Aborting.')
+  }
+
+  // run tests before release
+  step('Running tests ...')
+  if (!skipTests) {
+    await run(bin('jest'), ['--clearCache'])
+    await run('pnpm', ['test:once'])
+  } else {
+    console.log(`(skipped)`)
+  }
+
+  // build all packages with types
+  step('Building all packages ...')
+  if (!skipBuild) {
+    await run('pnpm', ['run', 'build', '--', '--release'])
+  } else {
+    console.log(`(skipped)`)
+  }
+
+  release({
+    checkGitStatus: false,
+  })
+}
+
+function bin(name) {
+  return path.resolve(__dirname, '../node_modules/.bin/' + name)
+}
---
+++ web/meta.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: 'platform',
+    message: 'Please choose the platform of your project:',
+    type: 'list',
+    choices: [
+      {
+        name: 'github',
+        value: 'github',
+        source: ['./github'],
+      },
+      {
+        name: 'gitlab',
+        value: 'gitlab',
+      },
+    ],
+  },
+]
